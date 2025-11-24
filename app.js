b6cc045db4fdf985206f025051e71c53f96d072e
Prince3963 <princepatel3963@gmail.com>
2025-11-25 01:26:02 +0530

    m

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
 const userRouter = require('./routes/userRoutes');
-const db = require('./config/db');
 
+const express = require('express');
 const app = express();
 app.use(express.json());
 
 // Database connection
+const db = require('./config/db');
 db();
 
 app.use("/api", userRouter);
